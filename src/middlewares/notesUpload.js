34bc3ed5435ed6b9874b890186a9445b2ab09ad6
Parmenide <nidedrogba@gmail.com>
2025-11-08 08:57:16 +0200

    directory cleanup and adaptation

---
+++ src/middlewares/notesUpload.js
@@ -0,0 +1,41 @@
+
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const { s3Config } = require('../modules/courses/utils/helpers');
+
+// File Filter for multer to check if the file is an image
+const fileFilter = (req, file, callback) => {
+
+    const allowedFileTypes = ['application/pdf', 'application/x-pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-powerpoint', 'application/vnd.openxmlformats-officedocument.presentationml.presentation'];
+
+    if (allowedFileTypes.includes(file.mimetype)) {
+        callback(null, true);
+    } else {
+        callback(null, false);
+    }
+};
+
+// Uploading file to aws
+const multerS3Config = multerS3({
+    s3: s3Config,
+    bucket: process.env.S3_BUCKET,
+    metadata: (req, file, callback) => {
+        callback(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, callback) => {
+        const folderName = 'notes/';
+        const fileName = file.originalname.toUpperCase().split(' ').join('-').replace(/[^a-zA-Z0-9.]/g, '-');
+        callback(null, folderName + fileName.replace(/\./g, '-[Shared by Quiz-Blog].'));
+    }
+});
+
+// Multer Configuration for uploading file on AWS S3 or locally
+const upload = multer({
+    storage: multerS3Config,
+    fileFilter: fileFilter,
+    limits: {
+        fileSize: 50000000 // 1000000 Bytes = 1 MB (50MB)
+    }
+});
+
+exports.notesUpload = upload;
---
+++ src/modules/comments/routes/questions-comments.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const { getQuestionsComments, getPaginatedComments, getPendingComments, getOneQuestionComment, getCommentsByQuiz, getCommentsByQuestion, createQuestionComment, approveRejectComment, deleteQuestionComment } = require('../controllers/questions-comments');
+const { auth, authRole } = require('../../../middlewares/auth');
+
+const router = express.Router();
+
+// GET routes
+router.get('/', getQuestionsComments);
+router.get('/paginated', authRole(['Admin', 'SuperAdmin']), getPaginatedComments);
+router.get('/pending', authRole(['Admin', 'SuperAdmin']), getPendingComments);
+router.get('/quiz/:id', getCommentsByQuiz);
+router.get('/question/:id', auth, getCommentsByQuestion);
+router.get('/:id', getOneQuestionComment);
+
+// POST routes
+router.post('/', auth, createQuestionComment);
+
+// PUT routes
+router.put('/approve-reject/:id/', authRole(['Admin', 'SuperAdmin']), approveRejectComment);
+
+// DELETE routes
+router.delete('/:id', authRole(['Admin', 'SuperAdmin']), deleteQuestionComment);
+
+module.exports = router;
