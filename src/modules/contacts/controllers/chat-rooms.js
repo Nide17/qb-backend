0860b9f3eef0b873e59dd9d51de89eb96a6f96e5
Parmenide <nidedrogba@gmail.com>
2025-12-21 21:04:51 +0200

    Refactored the socket

--- src/modules/contacts/controllers/chat-rooms.js
+++ src/modules/contacts/controllers/chat-rooms.js
@@ -191,6 +191,7 @@ exports.createOpenChatRoom = async (req, res) => {
         return res.status(201).json(expandedRoom);
 
     } catch (err) {
+        console.log(err);
         handleError(res, err);
     }
 };
